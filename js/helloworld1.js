aa015511a2f6df9478ae610fcdf159c92d2c2faa
boyoungna <kby52082933@gmail.com>
2021-12-01 17:20:59 +0900

    Initial commit

---
+++ js/helloworld1.js
@@ -0,0 +1,3 @@
+'use strict';   //무조건 써줄것.
+// java script 출력문 1
+console.log('hello world');
---
+++ js/varLet.js
@@ -0,0 +1,46 @@
+'use strict';
+//var & let : Javascript에서 변수 선언시 사용하는 키워드
+//var: 유연성을 가진다. => 선언전에도 사용 가능, 재선언 가능
+// var x, y, z;
+// x = 5;
+// y = 6;
+// z = x + y;
+// var x = 10;
+// var는 재선언 가능. let은 불가능.
+//let: 유연성이 없다. =. 사용전에 반드시 선언해야함. 재선언 불가능
+// {
+//     var b = 10;     //not block scope
+//     let a;
+//     a = 5;      // block scope 취급
+//     console.log(a);
+// }
+
+// console.log(b);
+
+//undefined
+var x;
+x = 10;     
+// 변수를 선언하고, 값을 초기화 하면, 리터럴의 데이터 타입에 따라 변수의 타입이 결정된다.
+console.log(typeof x);
+
+var y;
+console.log(typeof y);
+
+// 숫자와 문자의 혼합 연산
+var a = 5;
+var b = 10;
+var c = 'k';
+console.log(a + b + c);
+
+var a = 5;
+var b = '10';
+console.log(a+b);
+
+var a = '5';
+var b = '10';
+var c = a + b;
+console.log(c);
+
+console.log( 5 == '5');     //값만 취급:true
+console.log( 5 === '5');    //값과 타입까지 함께 취급:false
+
